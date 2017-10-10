d6600faa32fae340a16cc454a58e0867b9e2ead4
kc657 <kvn.0218@gmail.com>
2017-10-10 14:10:53 -0700

    Linked reducer to our action using types

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,9 +1,12 @@
+import { EMAIL_CHANGED } from '../actions.types'
+
 const initial_state = { email: '' }
 
 export default (state = initial_state, action) => {
   switch (action.type) {
+    case EMAIL_CHANGED:
+      
     default:
       return state;
-
   }
 }

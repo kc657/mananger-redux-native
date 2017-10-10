7c3f485b39ce34d0eab1b48938eaf4ad78fe9366
kc657 <kvn.0218@gmail.com>
2017-10-10 14:03:00 -0700

    Added initial blank state to authreducer and return default if no actiom

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,9 @@
+const initial_state = { email: '' }
+
+export default (state = initial_state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux'
+import AuthReducer from './AuthReducer'
 
 export default combineReducers({
-  placeholder: () => []
+  auth: AuthReducer
 })
+// auth state is controlled by our AuthReducer

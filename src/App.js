7dcb23436405c2a1d9c3ef60abc9032ac6279278
kc657 <kvn.0218@gmail.com>
2017-10-09 23:53:26 -0700

    initialized firebase

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import firebase from 'firebase'
+import reducers from './reducers'
 
 export default class App extends Component {
-  render() {
+  componentWillMount () {
+    const config = {
+      apiKey: 'AIzaSyAzGkkgIGDgOJO0QGGgz2DPCZ0Jp6VU6bA',
+      authDomain: 'manager-ce4cc.firebaseapp.com',
+      databaseURL: 'https://manager-ce4cc.firebaseio.com',
+      projectId: 'manager-ce4cc',
+      storageBucket: 'manager-ce4cc.appspot.com',
+      messagingSenderId: '716976161579'
+    }
+    firebase.initializeApp(config)
+  }
+  render () {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello
-
-
+          </Text>
+        </View>
       </Provider>
-    );
+    )
   }
 }

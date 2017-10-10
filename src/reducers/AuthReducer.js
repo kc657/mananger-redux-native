df1059868e8ede981545871a21bbe87caf7523b1
kc657 <kvn.0218@gmail.com>
2017-10-10 14:19:17 -0700

    { ...state, email: action.payload} creates new state with our payload

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -5,7 +5,7 @@ const initial_state = { email: '' }
 export default (state = initial_state, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
-      
+      return { ...state, email: action.payload}
     default:
       return state;
   }

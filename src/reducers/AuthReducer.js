ae8ce733941ef7b940c03f1f23c04e5aac704644
kc657 <kvn.0218@gmail.com>
2017-10-16 16:14:45 -0700

    Added login user success to reducer to return user payload

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,21 +1,25 @@
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
 } from '../actions/types'
 
 const initial_state = {
   email: '',
-  password: ''
+  password: '',
+  user: null
 }
 
 export default (state = initial_state, action) => {
   console.log(action);
-  
+
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload }
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload}
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload}
     default:
       return state
   }

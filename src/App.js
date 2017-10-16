9942169964bf39eed8f785d0d62e53d4f4918479
kc657 <kvn.0218@gmail.com>
2017-10-16 16:04:52 -0700

    added return (dispatch) to action

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import ReduxThunk from 'redux-thunk'
 import firebase from 'firebase'
 import reducers from './reducers'
 import LoginForm from './components/LoginForm'
@@ -19,8 +19,10 @@ export default class App extends Component {
     firebase.initializeApp(config)
   }
   render () {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     )
--- src/actions/index.js
+++ src/actions/index.js
@@ -19,6 +19,8 @@ export const passwordChanged = (text) => {
 }
 
 export const loginUser = ({email, password}) => {
-  firebase.auth().signInWithEmailAndPassword(email, password)
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
     .then((user) => console.log(user))
+  }
 }

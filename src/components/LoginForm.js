1a70c2f8168ae99b5f03dd1b356e54ef5f0065f1
kc657 <kvn.0218@gmail.com>
2017-10-16 16:26:11 -0700

    added login error messsage with text tag

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Text} from 'react-native'
 import  { connect } from 'react-redux' //bridge react and redux using this package
 import { emailChanged, passwordChanged, loginUser } from '../actions'
 import { Card, CardSection, Button, Input } from './common'
@@ -39,6 +40,10 @@ class LoginForm extends Component {
         />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.props.error}
+        </Text>
+
         <CardSection>
           <Button onPress={this.onButtonPress}>
             Login
@@ -49,11 +54,17 @@ class LoginForm extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    email: state.auth.email,
-    password: state.auth.password
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
+}
+
+const mapStateToProps = ({auth}) => {
+  const { email, password, error } = auth
+  return { email, password, error}
 }
 
 export default connect(mapStateToProps, {

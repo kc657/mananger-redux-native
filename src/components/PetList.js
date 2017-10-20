d0e99fa4aa7ee17b6cedd7d3bd8ffed994cd7407
kc657 <kvn.0218@gmail.com>
2017-10-20 14:41:01 -0700

    createDataSource in pet list

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Text, View } from 'react-native'
+import { Text, View, ListView } from 'react-native'
 import { petFetch } from '../actions'
 
 class PetList extends Component {
 
   componentWillMount () {
     this.props.petFetch()
+
+    this.createDataSource(this.props)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.createDataSource(nextProps)
+  }
+
+  createDataSource = ({ pets }) => {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    })
+
+    this.dataSource = ds.clonseWithRows(pets)
   }
 
   render () {

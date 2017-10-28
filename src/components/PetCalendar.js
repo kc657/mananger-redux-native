e519f6dce42d6a8aa8dea8a4c6f621445c1f8697
kc657 <kvn.0218@gmail.com>
2017-10-27 23:40:46 -0700

    PetCalender with common imported

--- src/components/PetCalendar.js
+++ src/components/PetCalendar.js
@@ -4,7 +4,13 @@ import { connect } from 'react-redux'
 import { Card, CardSection, Button, Input, Spinner } from './common'
 
 class PetCalender extends Component {
+  render () {
+    return (
+      <CardSection>
 
+      </CardSection>
+    )
+  }
 }
 
 export default PetCalendar

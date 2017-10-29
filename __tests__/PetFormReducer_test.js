8e9494bdfa6458b3185364d3288aac33feac0036
kc657 <kvn.0218@gmail.com>
2017-10-28 21:12:25 -0700

    Added two tests for PET_FORM_UPDATE

--- __tests__/PetFormReducer_test.js
+++ __tests__/PetFormReducer_test.js
@@ -1,7 +1,7 @@
 import reducer from '../src/reducers/PetFormReducer'
 import * as types from '../src/actions/types'
 
-describe('todos reducer', () => {
+describe('Pet Form Reducer', () => {
   it('should return the initial state', () => {
     expect(reducer(undefined, {})).toEqual(
       {
@@ -9,6 +9,32 @@ describe('todos reducer', () => {
         phone: '',
         checkupDate: '',
         weight: ''
+      }
+    )
+  }),
+
+  it('should handle PET_FORM_UPDATE for name input', () => {
+    expect(
+      reducer([], {
+        type: types.PET_FORM_UPDATE,
+        payload: { prop: 'name', value: 'Kevin Chen'}
+      })
+    ).toEqual(
+      {
+        name: 'Kevin Chen'
+      }
+    )
+  })
+
+  it('should handle PET_FORM_UPDATE for date input', () => {
+    expect(
+      reducer([], {
+        type: types.PET_FORM_UPDATE,
+        payload: { prop: 'calendar', value: 'Saturday'}
+      })
+    ).toEqual(
+      {
+        calendar: 'Saturday'
       }
     )
   })

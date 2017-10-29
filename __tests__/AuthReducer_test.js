635557efcb2dbc1bf65b527da73f01df077796f6
kc657 <kvn.0218@gmail.com>
2017-10-28 20:38:37 -0700

    Added reducertest file

--- __tests__/AuthReducer_test.js
+++ __tests__/AuthReducer_test.js
@@ -5,8 +5,4 @@ import App from '../src/App'
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <App />
-  )
-})
+describe('')
